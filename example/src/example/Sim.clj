c4c9e51f38a85c9314db239fdfe026e233f4d03a
mars0i <mabrams@uab.edu>
2019-03-23 14:05:06 -0500

    added FIXME comment--need to fix comment

--- example/src/example/Sim.clj
+++ example/src/example/Sim.clj
@@ -25,6 +25,7 @@
 ;; Generate Sim class as subclass of SimState using genclass, with an init 
 ;; function, import statement, and Bean/MASON field accessors.
 ;; To see what code will be generated, try this in a repl:
+;; FIXME:
 ;;    (require '[utils.defsim :as defsim])
 ;;    (pprint (macroexpand-1 '<insert defsim call>))
 
