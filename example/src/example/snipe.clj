e0eb11c3e5c386fc759bd260407da5d987bcac04
mabrams <mabrams@uab.edu>
2019-03-23 23:45:33 -0500

    misc simplifications, e.g. removed mush-pref field

--- example/src/example/snipe.clj
+++ example/src/example/snipe.clj
@@ -25,7 +25,7 @@
 ;; The two atom fields at the end are there solely for interactions with the UI.
 ;; Propertied/properties is used by GUI to allow inspectors to follow a fnlly updated agent.
 
-(defrecord RSnipe [id mush-pref energy subenv x y circled$ cfg-data$] ; r-snipe that prefers small mushrooms
+(defrecord RSnipe [id energy subenv x y circled$ cfg-data$]
   Propertied
   (properties [original-snipe] (make-properties id cfg-data$))
   Object
@@ -37,7 +37,7 @@
 (defn make-r-snipe
   [rng cfg-data$ energy subenv new-id x y]
   (let [extreme-pref (:extreme-pref @cfg-data$)]
-      (RSnipe. new-id extreme-pref energy subenv x y (atom false) cfg-data$)))
+      (RSnipe. new-id energy subenv x y (atom false) cfg-data$)))
 
 (defn make-newborn-r-snipe
   [rng cfg-data$ subenv new-id x y]
@@ -54,13 +54,13 @@
 
 ;; Used by GUI to allow inspectors to follow a functionally updated agent.
 ;; i.e. normally you can double-click on an agent, and its internal state will
-;; be displayed while a circle follows it around.  The normal implementation of
-;; this in MASON assumes that the JVM identity of an agent never changes; you
-;; can simply track it using a pointer.  Since we are defining agents using defrecord,
-;; we get all of the conveniences that Clojure provides for defrecords, when you update
-;; a defrecord, you create a new objet with a new identity, and MASON can't track
-;; that across updates unless you give it a special class that it will use to track
-;; and agent over time.  This is what this function does.
+;; be displayed (while a circle follows it around, if you implement that in UI code).
+;; The normal implementation of this in MASON assumes that the JVM identity of an 
+;; agent never changes; you can simply track it using a pointer.  Since we are defining 
+;; agents using defrecord, we get all of the conveniences that Clojure provides for 
+;; defrecords, when you update a defrecord, you create a new objet with a new identity,
+;; and MASON can't track that across updates unless you give it a special class that it 
+;; will use to track and agent over time.  This is what this function does.
 ;; (The code below makes use of the fact that in Clojure, vectors can be treated as 
 ;; functions ;; of indexes, returning the indexed item; that keywords such as :x can be
 ;; treated as functions of maps; and that defrecords such as snipes can be treated as maps.)
@@ -68,21 +68,20 @@
   "Return a Properties subclass for use by Propertied's properties method so
   that certain fields can be displayed in the GUI on request."
   [id cfg-data$]
-  ;; These definitions need to be coordinated by hand:
-  (let [kys [:energy :mush-pref :subenv :x :y :circled$] ; TODO CHANGE FOR NEW FIELDS
-        circled-idx 5 ; HARDCODED INDEX for circled$ field              ; TODO CHANGE FOR NEW FIELDS
-        descriptions ["Energy is what snipes get from mushrooms."       ; TODO CHANGE FOR NEW FIELDS
-                      "Preference for large (positive number) or small (negative number) mushrooms."
-                      "Name of snipe's subenv"
+  ;; THESE DEFINITIONS NEED TO BE COORDINATED BY HAND:
+  (let [kys [:circled$ :x :y :energy :subenv] ; TODO CHANGE FOR NEW FIELDS
+        circled-idx 0 ; HARDCODED INDEX for circled$ field
+        descriptions ["Boolean indicating whether circled in GUI"       ; TODO CHANGE FOR NEW FIELDS
                       "x coordinate in underlying grid"
                       "y coordinate in underlying grid"
-                      "Boolean indicating whether circled in GUI"]
-        types [java.lang.Double java.lang.Double java.lang.String java.lang.Integer java.lang.Integer ; TODO CHANGE FOR NEW FIELDS
-               java.lang.Boolean]                                                                     ; TODO CHANGE FOR NEW FIELDS
-        read-write [false false false false false true] ; allow user to turn off circled in UI ; TODO CHANGE FOR NEW FIELDS
+                      "Energy is what snipes get from mushrooms"
+                      "Name of sub-environment"]
+        types [java.lang.Boolean java.lang.Integer java.lang.Integer java.lang.Double java.lang.String] ; TODO CHANGE FOR NEW FIELDS
+        read-write [true false false false false] ; allow user to turn off circled in UI ; TODO CHANGE FOR NEW FIELDS
         names (mapv name kys)
         num-properties (count kys)
         hidden     (vec (repeat num-properties false)) ; no properties specified here are to be hidden from GUI
+        ;; THIS IS THE KEY MOVE: Look up the current version of this agent by id:
         get-curr-snipe (fn [] ((:snipe-map (:popenv @cfg-data$)) id))] ; find current version of this snipe
     (reset! (:circled$ (get-curr-snipe)) true) ; make-properties is only called by inspector, in which case highlight snipe in UI
     ;; Aside from the last line, the code below can probably be used as-is for any agent:
