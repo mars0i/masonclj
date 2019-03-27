988f00471f60cf6859a5430108346af5025b6ba9
mabrams <mabrams@uab.edu>
2019-03-27 00:42:17 -0500

    Modified html table write to write to current dir
    Previously, it was writing to the Leiningen dir of the masonclj
    library!

--- src/masonclj/params.clj
+++ src/masonclj/params.clj
@@ -215,7 +215,9 @@
      ;; GENERATE HTML TABLE DOCUMENTING VARIABLES POSSIBLY VISIBLE IN GUI
      ;; Note this will only happen whem Sim.clj is recompiled.
      (println "Writing GUI vars html table to file" gui-vars-html-filename ".")
-     (spit gui-vars-html-filename gui-vars-html#)
+     ;; Write the html file to the current directory:
+     (spit (str (.getCanonicalPath (clojure.java.io/file ".")) "/" gui-vars-html-filename) ; DOES THIS WORK IN WINDOWS?  Should it be more flexible?
+           gui-vars-html#)
 
      ;; GENERATE CODE FOR Sim.clj:
      `(do
