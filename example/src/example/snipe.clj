f2c1fd94f69aa20ce54913c0f4be65a9b5b528c3
mabrams <mabrams@uab.edu>
2019-03-29 23:35:09 -0500

    get-curr-obj now takes original agent arg with explicit id field
    and you don't have to make the function a closure over id
    though you can make it a closure over whatever you want.
    
    and an id field is still required, for the toString, but I am letting
    user supply it.

--- example/src/example/snipe.clj
+++ example/src/example/snipe.clj
@@ -34,8 +34,8 @@
   "Return a function that can be the value of getObject in Properties,
   i.e. that will return the current time-slice of a particular snipe.
   The function returned will be a closure over snipe-id and cfg-data$."
-  [id cfg-data$] ; pass cfg-data$ and not @cfg-data$ so the fn always uses the latest data.
-  (fn [] ((:snipe-map (:popenv @cfg-data$)) id)))
+  [cfg-data$ original-agent] ; pass cfg-data$ and not @cfg-data$ so the fn always uses the latest data.
+  (fn [] ((:snipe-map (:popenv @cfg-data$)) (:id original-agent))))
 
 ;; The two atom fields at the end are there solely for interactions with the UI.
 ;; Propertied/properties is used by GUI to allow inspectors to follow a fnlly updated agent.
@@ -46,8 +46,8 @@
 ;  Object
 ;  (toString [this] (str "<RSnipe #" id ">")))
 
-(props/defagent RSnipe [energy subenv x y cfg-data$] 
-  (make-get-curr-obj id cfg-data$)
+(props/defagent RSnipe [id energy subenv x y cfg-data$] 
+  (partial make-get-curr-obj cfg-data$)
   [[:energy    java.lang.Double "Energy is what snipes get from mushrooms."]
    [:x         java.lang.Integer "x coordinate in underlying grid"]
    [:y         java.lang.Integer "y coordinate in underlying grid"]])
--- src/masonclj/properties.clj
+++ src/masonclj/properties.clj
@@ -47,7 +47,7 @@
   element is a Java type for the field, and the third element is a string 
   describing the field.  This function assumes that the defrecord contains a
   field named circled$ containing an atom containing a boolean."
-  [id get-curr-obj & fields]
+  [get-curr-obj & fields]
   (let [data-field-keys (map data-field-key fields)
         data-types (map data-type fields)
         data-descriptions (map data-description fields)
@@ -60,7 +60,8 @@
         num-properties (count property-keys)
         names (mapv name property-keys)
         are-writeable (vec (cons true (repeat num-data-fields false)))
-        hidden        (vec (repeat num-properties false))] ; no properties specified here are to be hidden from GUI
+        hidden        (vec (repeat num-properties false)) ; no properties specified here are to be hidden from GUI
+        id (:id (get-curr-obj))] ; If the original agent doesn't have a field named "id", this will be nil.
     (reset! (:circled$ (get-curr-obj)) true) ; make-properties is only called by inspector, in which case highlight snipe in UI
     (proxy [Properties] [] ; the methods below are expected by Properties
       (getObject [] (get-curr-obj))
@@ -80,19 +81,17 @@
       (isReadWrite [i] (are-writeable i))
       (isVolatile [] false)
       (numProperties [] num-properties)
-      (toString [] (str "<SimpleProperties for agent with id=" id ">")))))
+      (toString [] (str "<SimpleProperties for agent " id ">")))))
 
-;; TODO Why am I passing id?  The properties method takes original-snipe (?), so
-;; I can get the id from there.
-;; DO I REALLY WANT id AND circled$ AS LITERALS, i.e. THEY CAN BE CAPTURED?
-;; TODO SHOULD id be added here?
+;; DO I REALLY WANT circled$ AS A LITERAL, i.e. CAN BE CAPTURED?
 (defmacro defagent
-  "FIXME"
+  "fields must include a field named id; this is used in the toString
+  method for this agent."
   [agent-type fields get-curr-obj-maker gui-fields-specs & addl-defrecord-args] ; function-maker and not function so it can capture id inside 
-  `(defrecord ~agent-type [~'circled$ ~'id ~@fields]
+  `(defrecord ~agent-type [~'circled$ ~@fields]
      Propertied
        (properties [original-snipe#]
-         (make-properties ~'id ~get-curr-obj-maker ~@gui-fields-specs))
+         (make-properties (~get-curr-obj-maker original-snipe#) ~@gui-fields-specs))
      Object
        (toString [_#] (str "<" '~agent-type " #" ~'id ">"))
      ~@addl-defrecord-args))
