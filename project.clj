fa289e9955c5272867765a76f46114a063b156de
mars0i <mabrams@uab.edu>
2019-04-15 11:17:44 -0500

    added description and inc'ed version to 0.2

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject mars0i/masonclj "0.1.0"
-  :description "FIXME: write description"
+(defproject mars0i/masonclj "0.2.0"
+  :description "masonclj is a small library providing functions and macros
+                to make it easier to use the MASON ABM library in Clojure."
   :url "https://github.com/mars0i/masonclj"
   :license {:name "LGPL 3.0"
             :url "https://www.gnu.org/licenses/lgpl.html"}
